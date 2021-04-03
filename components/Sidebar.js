5caf0dd114b55807f7e07537f5af12a222de77b0
Kim Krog <kimkrog@Kims-MBP.localdomain>
2021-04-03 20:01:22 +0200

    Sidebar setup

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,12 +3,49 @@ import styled from "styled-components";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchIcon from "@material-ui/icons/Search";
+import * as EmailValidator from "email-validator";
+import { auth, db } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
+import Chat from "./Chat";
 
 function sidebar() {
+  const [user] = useAuthState(auth);
+  const userChatRef = db
+    .collection("chats")
+    .where("users", "array-contains", user.email);
+
+  const [chatsSnapshot] = useCollection(userChatRef);
+
+  const createChat = () => {
+    const input = prompt(
+      "Please enter an email adress for the user you wish to chat with"
+    );
+
+    if (!input) return null;
+
+    if (
+      EmailValidator.validate(input) &&
+      !chatAlreadyExists(input) &&
+      input !== user.email
+    ) {
+      // Add the chat to DB 'chats' collection if it doesn't already exist and is valid
+      db.collection("chats").add({
+        users: [user.email, input],
+      });
+    }
+  };
+
+  const chatAlreadyExists = (recipientEmail) =>
+    !!chatsSnapshot?.docs.find(
+      (chat) =>
+        chat.data().users.find((user) => user === recipientEmail)?.length > 0
+    );
+
   return (
     <Container>
       <Header>
-        <UserAvatar />
+        <UserAvatar onClick={() => auth.signOut()} src={user.photoURL} />
 
         <IconsContainer>
           <IconButton>
@@ -23,9 +60,12 @@ function sidebar() {
         <SearchIcon />
         <SearchInput placeholder="Search in chats" />
       </Search>
-      <SidebarButton>Start a new chat</SidebarButton>
+      <SidebarButton onClick={createChat}>Start a new chat</SidebarButton>
 
       {/* List of Chats */}
+      {chatsSnapshot?.docs.map((chat) => (
+        <Chat key={chat.id} id={chat.id} users={chat.data().users} />
+      ))}
     </Container>
   );
 }
---
+++ pages/chat/[id].js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import Head from "next/head";
+import Sidebar from "../../components/Sidebar";
+
+function Chat() {
+  return (
+    <Container>
+      <Head>
+        <title>Chat</title>
+      </Head>
+      <Sidebar />
+      <ChatContainer></ChatContainer>
+    </Container>
+  );
+}
+
+export default Chat;
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const ChatContainer = styled.div``;
